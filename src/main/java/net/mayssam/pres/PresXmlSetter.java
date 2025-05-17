f840e951bbb65bdfb9e19a0c18fb2ce6035e332c
MayssamSe <165916486+MayssamSe@users.noreply.github.com>
2025-05-17 12:30:44 +0200

    Update PresXmlSetter.java

--- src/main/java/net/mayssam/pres/PresXmlSetter.java
+++ src/main/java/net/mayssam/pres/PresXmlSetter.java
@@ -11,8 +11,8 @@ public class PresXmlSetter {
         XmlApplicationContext ctx = new XmlApplicationContext("beans.xml");
         IDao dao = (IDao) ctx.getBean("dao");
         IMetier metier = (IMetier) ctx.getBean("metierImpl3");
-        System.out.println("(setter) DAO data = " + dao.getData());             // 42.0
-        System.out.println("(setter) Metier calcul = " + metier.calcul());      // (42+10)*2 = 104.0
+        System.out.println("(setter) DAO data = " + dao.getData());             
+        System.out.println("(setter) Metier calcul = " + metier.calcul());      
 
     }
 }
